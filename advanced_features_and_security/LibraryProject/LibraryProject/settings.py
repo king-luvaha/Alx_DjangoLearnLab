b7bbb036e0d6023771c8a1322f4618431e8c4c55
Luvaha <luvaha456@gmail.com>
2025-07-21 23:43:27 +0300

    Updated LibraryProject/settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -166,3 +166,4 @@ SECURE_SSL_REDIRECT = True  # Important in production
 SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Apply to all subdomains
 SECURE_HSTS_PRELOAD = True  # Allow browser preload list inclusion
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
