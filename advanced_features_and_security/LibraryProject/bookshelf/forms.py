3c54d76a5f4f9b605fd27a38a4715974091cde59
Luvaha <luvaha456@gmail.com>
2025-07-21 23:23:45 +0300

    Updated bookshelf/forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,3 +2,8 @@ from django import forms
 
 class SearchForm(forms.Form):
     query = forms.CharField(required=False, max_length=100, label='Search Title')
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(label='Your Name', max_length=100)
+    email = forms.EmailField(label='Email')
+    message = forms.CharField(label='Message', widget=forms.Textarea)
