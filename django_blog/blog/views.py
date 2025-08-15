09f2c0a51a37ed0998ef7583f8f1b1f4e4e45a20
Luvaha <luvaha456@gmail.com>
2025-08-15 13:58:37 +0300

    Commit

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 # List view
@@ -26,7 +27,7 @@ class PostCreateView(CreateView):
         return super().form_valid(form)
 
 # Update view
-class PostUpdateView(UpdateView):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     template_name = 'blog/post_form.html'
     fields = ['title', 'content']
@@ -34,9 +35,16 @@ class PostUpdateView(UpdateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
 
-# Delete view
-class PostDeleteView(DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
-    success_url = reverse_lazy('post-list')  # Change 'post-list' to your actual list view name
+    success_url = reverse_lazy('post-list')   # Change 'post-list' to your actual list view name
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
