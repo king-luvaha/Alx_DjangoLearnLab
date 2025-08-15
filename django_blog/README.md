748d9225afae00344f3cd88cc6ec853a089e5db8
Luvaha <luvaha456@gmail.com>
2025-08-15 13:40:17 +0300

    Commit

---
+++ django_blog/README.md
@@ -0,0 +1,15 @@
+# Django Blog - CRUD Functionality
+
+## Features
+- View all blog posts
+- View single post details
+- Create new posts (logged-in users only)
+- Edit and delete posts (only by post author)
+- User-friendly templates
+
+## URLs
+- `/posts/` → list of posts
+- `/posts/new/` → create post
+- `/posts/<id>/` → post details
+- `/posts/<id>/edit/` → edit post
+- `/posts/<id>/delete/` → delete post
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from .forms import PostForm
 
-# Create your views here.
+# List all posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']  # newest first
+
+# View single post details
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+# Create new post (only logged in)
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# Update post (only by author)
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Delete post (only by author)
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/posts/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
