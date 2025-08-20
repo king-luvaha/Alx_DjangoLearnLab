9a55551d43141364a7a68897dcdeddfd5e5e2943
Luvaha <luvaha456@gmail.com>
2025-08-20 17:53:05 +0300

    Commit

--- social_media_api/notifications/models.py
+++ social_media_api/notifications/models.py
@@ -17,8 +17,18 @@ class Notification(models.Model):
         on_delete=models.CASCADE
     )
     verb = models.CharField(max_length=255)
+    
+    #Generic foreign key for target object (e.g., post, comment, follow)
+    target_content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True
+    )
+    target_object_id = models.PositiveIntegerField(null=True, blank=True)
+    target = GenericForeignKey("target_content_type", "target_object_id")
+
     timestamp = models.DateTimeField(auto_now_add=True)
-    is_read = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"{self.actor} {self.verb} {self.recipient}"
+        return f"{self.actor} {self.verb} {self.target} → {self.recipient}"
