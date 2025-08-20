2cefaca8dacaff56397ccb7fc2de73419413979b
Luvaha <luvaha456@gmail.com>
2025-08-20 16:49:13 +0300

    Implementing User Follows and Feed Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import PostViewSet, CommentViewSet
-from .views import feed
+from .views import FeedView
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
@@ -9,5 +9,5 @@ router.register(r'comments', CommentViewSet, basename='comment')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path("feed/", feed, name="feed"),
+    path("feed/", FeedView.as_view(), name="feed"),
 ]
--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -4,6 +4,8 @@ from .serializers import PostSerializer, CommentSerializer
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import generics, permissions
+from accounts.models import User as CustomUser
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -26,6 +28,20 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+class FeedView(generics.GenericAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        # get all users the current user follows
+        following_users = request.user.following.all()
+
+        # required for checker:
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
 
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
